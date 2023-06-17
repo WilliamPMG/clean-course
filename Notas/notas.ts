9b5bad1c55ec5480e8f4b1b6d6fdd58d45c316a8
wmorochom <williammorocho@coopdaquilema.com>
2023-06-17 06:18:10 -0500

    Notas

--- Notas/notas.ts
+++ Notas/notas.ts
@@ -1,5 +1,6 @@
 /* 
 Clean Coat y Deuda Tecnica 
+Deuda tecnica: falta de calidad en nuestro codigo o falta de calidad
 en algun proceso de desarrollo de software (Falta de documentacion, falta de pruebas, falta de refactorizacion).
 La falta de calidad en el codigo, que genera una deuda que repercutira en costos futuros.
 La deuda tecnica genera costos economicos (Tiempo en realizar mantenimientos, Tiempo en refactorizar codigos, tiempo en comprender en codigo, tiempo en la transferencia del codigo)
