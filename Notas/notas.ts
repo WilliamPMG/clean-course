aa1fc1931e98ee08f81d29ee57c72801f8fe1031
wmorochom <williammorocho@coopdaquilema.com>
2023-06-17 06:17:52 -0500

    Avance 1 clase

---
+++ Notas/notas.ts
@@ -0,0 +1,11 @@
+/* 
+Clean Coat y Deuda Tecnica 
+en algun proceso de desarrollo de software (Falta de documentacion, falta de pruebas, falta de refactorizacion).
+La falta de calidad en el codigo, que genera una deuda que repercutira en costos futuros.
+La deuda tecnica genera costos economicos (Tiempo en realizar mantenimientos, Tiempo en refactorizar codigos, tiempo en comprender en codigo, tiempo en la transferencia del codigo)
+
+
+
+
+
+*/
