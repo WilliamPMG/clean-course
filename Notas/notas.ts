8261020da4757d914d6072970bb7b39ce76dbbab
wmorochom <williammorocho@coopdaquilema.com>
2023-06-17 06:26:04 -0500

    Sentido semantico

--- Notas/notas.ts
+++ Notas/notas.ts
@@ -5,6 +5,13 @@ en algun proceso de desarrollo de software (Falta de documentacion, falta de pru
 La falta de calidad en el codigo, que genera una deuda que repercutira en costos futuros.
 La deuda tecnica genera costos economicos (Tiempo en realizar mantenimientos, Tiempo en refactorizar codigos, tiempo en comprender en codigo, tiempo en la transferencia del codigo)
 
+Para pagar la deuda tecnica se paga con la refactorizacion
+Refactorizar es simplemente un proceso que tiene como objetivo mejorar el codigo sin alterar su comportamiento para que sea mas entendible y tolerante a cambios.
+
+Clean Code
+- Nombres pronunciables y expresivos
+- Ausencia de informacion tecnica en  nombres MAL => class UserClass Correcto=> class User
+interface UserInterface Correcto=> interface User
 
 
 
